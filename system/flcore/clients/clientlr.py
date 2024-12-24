7ad933ad4f8ec1edfc3dce09e1964929fda310eb
ReverseRain <12210626@mail.sustech.edu.cn>
2024-12-24 19:49:06 +0800

    testing final

--- system/flcore/clients/clientlr.py
+++ system/flcore/clients/clientlr.py
@@ -5,21 +5,28 @@ import time
 import numpy as np
 import torch.nn.functional as F
 import torch.nn.utils.parametrize as parametrize 
+from sklearn.preprocessing import label_binarize
+from sklearn import metrics
 from flcore.clients.clientbase import Client
 
-class LoRAParametrization(nn.Module):  
+    
+class LoRAadpater(nn.Module):  
     def __init__(self, features_in, features_out, rank=3, alpha=1, device='cpu'):  
         super().__init__()  
         # 论文第4.1节：  
         # 我们使用随机高斯初始化A，并将B初始化为零，所以ΔW = BA在训练开始时为零  
-        self.lora_A = nn.Parameter(torch.zeros((rank, features_in)).to(device))  # 初始化为零的低秩矩阵A  
-        self.lora_B = nn.Parameter(torch.zeros((features_out, rank)).to(device))  # 初始化为零的低秩矩阵B  
+        self.lora_A = nn.Parameter(torch.zeros((rank, features_in)).to(torch.float).to(device))
+        self.lora_B = nn.Parameter(torch.zeros((features_out, rank)).to(torch.float).to(device))  # 初始化为零的低秩矩阵B 
+        
         nn.init.normal_(self.lora_A, mean=0, std=1)  # 对A进行标准正态分布初始化  
+
+        self.device=device
   
-        self.scale = alpha / rank  # 缩放因子  
+        self.scale = 0.33  # 缩放因子  
   
-    def forward(self, original_weights): 
-        return original_weights + torch.matmul(self.lora_B, self.lora_A) * self.scale  
+    def forward(self, x): 
+        weight=torch.matmul(self.lora_B, self.lora_A).to(self.device)
+        return   torch.matmul(x,weight.T)
 
 class clientLR(Client):
     def __init__(self, args, id, train_samples, test_samples, **kwargs):
@@ -29,11 +36,17 @@ class clientLR(Client):
 
         features_in=self.model.head.in_features
         features_out=self.model.head.out_features
-        
+        self.client2global=0
         # self.model.head.weight.requires_grad=False
         # self.model.head.bias.requires_grad=False
-        parametrize.register_parametrization(  
-            self.model.head, "weight", LoRAParametrization(features_in, features_out,3,1,self.device) )
+        self.adpater=LoRAadpater(features_in, features_out,3,1,self.device)
+
+        self.opt_adpater = torch.optim.SGD(self.adpater.parameters(), lr=self.learning_rate)
+        self.learning_rate_scheduler_adpater = torch.optim.lr_scheduler.ExponentialLR(
+            optimizer=self.opt_adpater, 
+            gamma=args.learning_rate_decay_gamma
+        )
+
         
     
     def train(self):
@@ -46,10 +59,13 @@ class clientLR(Client):
         max_local_epochs = self.local_epochs
         if self.train_slow:
             max_local_epochs = np.random.randint(1, max_local_epochs // 2)
-        # in this step update global model  (include base and global head) 
-        for param in self.model.base.parameters():
-            param.requires_grad = True
 
+        u=0.3
+        # 第二次训练的过程中冻结模型  而训练adapter  
+        for param in self.model.parameters():
+            param.requires_grad = False
+        for param in self.adpater.parameters():
+            param.requires_grad = True
         
         for epoch in range(max_local_epochs):
             for i, (x, y) in enumerate(trainloader):
@@ -60,24 +76,132 @@ class clientLR(Client):
                 y = y.to(self.device)
                 if self.train_slow:
                     time.sleep(0.1 * np.abs(np.random.rand()))
-                output = self.model(x)
-                loss = self.loss(output, y)
+                
+                tem = self.model.base(x)
+                output1=self.adpater(tem)
+                output2=self.model.head(tem)
+                loss=self.loss(output2,y)+self.adpater.scale*self.loss(output1,y)+self.client2global*0.2
+
+                self.opt_adpater.zero_grad()
+                self.optimizer.zero_grad()
+                loss.backward()
+                self.opt_adpater.step()
+
+        # 第一次训练的过程中冻结adapter 而训练模型    
+        for param in self.adpater.parameters():
+            param.requires_grad = False
+        for param in self.model.parameters():
+            param.requires_grad = True
+        
+        for epoch in range(max_local_epochs):
+            for i, (x, y) in enumerate(trainloader):
+                if type(x) == type([]):
+                    x[0] = x[0].to(self.device)
+                else:
+                    x = x.to(self.device)
+                y = y.to(self.device)
+                if self.train_slow:
+                    time.sleep(0.1 * np.abs(np.random.rand()))
+
+                tem = self.model.base(x)
+                output1=self.adpater(tem)
+                output2=self.model.head(tem)
+                loss=self.loss(output2,y)+self.adpater.scale*self.loss(output1,y)
+
+                self.opt_adpater.zero_grad()
                 self.optimizer.zero_grad()
                 loss.backward()
                 self.optimizer.step()
+
+
         
         
         
 
         if self.learning_rate_decay:
             self.learning_rate_scheduler.step()
-            # self.learning_rate_scheduler_ghead.step()
+            self.learning_rate_scheduler_adpater.step()
 
         self.train_time_cost['num_rounds'] += 1
         self.train_time_cost['total_cost'] += time.time() - start_time
 
 
     def set_parameters(self, global_head):
-        self.model.head.weight.data=global_head.weight.data.clone()
-        self.model.head.bias.data=global_head.bias.data.clone()
+        gm = torch.cat([p.data.view(-1) for p in global_head.parameters()], dim=0)
+        pm = torch.cat([p.data.view(-1) for p in self.model.head.parameters()], dim=0)
+        self.client2global=torch.norm(pm-gm, p=2)
+        for new_param, old_param in zip(global_head.parameters(), self.model.head.parameters()):
+            old_param.data = new_param.data.clone()
+        
+        
+    def test_metrics(self):
+        testloaderfull = self.load_test_data()
+        # self.model = self.load_model('model')
+        # self.model.to(self.device)
+        self.model.eval()
 
+        test_acc = 0
+        test_num = 0
+        y_prob = []
+        y_true = []
+        
+        with torch.no_grad():
+            for x, y in testloaderfull:
+                if type(x) == type([]):
+                    x[0] = x[0].to(self.device)
+                else:
+                    x = x.to(self.device)
+                y = y.to(self.device)
+                tem=self.model.base(x)
+                # print(tem.shape,self.adpater)
+                output = self.model.head(tem)+self.adpater.scale*self.adpater(tem)
+
+                test_acc += (torch.sum(torch.argmax(output, dim=1) == y)).item()
+                test_num += y.shape[0]
+
+                y_prob.append(output.detach().cpu().numpy())
+                nc = self.num_classes
+                if self.num_classes == 2:
+                    nc += 1
+                lb = label_binarize(y.detach().cpu().numpy(), classes=np.arange(nc))
+                if self.num_classes == 2:
+                    lb = lb[:, :2]
+                y_true.append(lb)
+
+        # self.model.cpu()
+        # self.save_model(self.model, 'model')
+
+        y_prob = np.concatenate(y_prob, axis=0)
+        y_true = np.concatenate(y_true, axis=0)
+
+        auc = metrics.roc_auc_score(y_true, y_prob, average='micro')
+        
+        return test_acc, test_num, auc
+
+    def train_metrics(self):
+        trainloader = self.load_train_data()
+        # self.model = self.load_model('model')
+        # self.model.to(self.device)
+        self.model.eval()
+
+        train_num = 0
+        losses = 0
+        with torch.no_grad():
+            for x, y in trainloader:
+                if type(x) == type([]):
+                    x[0] = x[0].to(self.device)
+                else:
+                    x = x.to(self.device)
+                y = y.to(self.device)
+                tem=self.model.base(x)
+                output = self.model.head(tem)+self.adpater.scale*self.adpater(tem)
+
+                loss = self.loss(output, y)
+                train_num += y.shape[0]
+                losses += loss.item() * y.shape[0]
+
+        # self.model.cpu()
+        # self.save_model(self.model, 'model')
+
+        return losses, train_num
+        
